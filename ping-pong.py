584b5c6b778a353aa413a8c6fc561b72a9a615ca
MironBorodin <godrin4ik@gmail.com>
2026-05-07 18:57:31 +0300

    Update ping-pong.py
    добавление столкновения и отталкивания от объектов и границ

--- ping-pong.py
+++ ping-pong.py
@@ -4,12 +4,14 @@ win_width = 600
 win_height = 500
 BACK = (200, 255, 255)
 game = True
+speed_x = 3
+speed_y = 3
 
 class GameSprite(pygame.sprite.Sprite):
     def __init__(self, image, speed, x, y, w, h):
         super().__init__()
         self.image = pygame.transform.scale(
-            pygame.image.load(image), (w, h-8)
+            pygame.image.load(image), (w-8, h-8)
             )
         self.speed = speed
         self.rect = pygame.Rect(x, y, w, h)
@@ -18,7 +20,7 @@ class GameSprite(pygame.sprite.Sprite):
 
     def reset(self):
         pygame.draw.rect(window, BACK, self.rect)
-        window.blit(self.image, (self.rect.x, self.rect.y+4))
+        window.blit(self.image, (self.rect.x+4, self.rect.y+4))
 
 class Player(GameSprite):
     def update_r(self):
@@ -41,14 +43,21 @@ clock = pygame.time.Clock()
 
 player_l = Player('rocket.jpg', 4, 30, 200, 50, 158)
 player_r = Player('rocket.jpg', 4, 520, 200, 50, 158)
-ball = GameSprite('ball.png', 4, 200, 200, 50, 50)
+ball = GameSprite('ball.png', 4, 200, 200, 58, 58)
 
 while game:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             game = False
 
+    if ball.rect.y >= win_height-50 or ball.rect.y <= 0:
+        speed_y *= -1
 
+    if pygame.sprite.collide_rect(ball, player_l) or pygame.sprite.collide_rect(ball, player_r):
+        speed_x *= -1
+
+    ball.rect.x += speed_x
+    ball.rect.y += speed_y
     player_l.reset()
     player_r.reset()
     ball.reset()
